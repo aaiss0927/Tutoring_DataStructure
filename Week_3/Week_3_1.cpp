e4d3401c7144b61d375ddf60d480d9e3d544e6c9
aaiss0927 <aaiss0927@naver.com>
2024-03-20 22:40:02 +0900

    Week_3_1

---
+++ Week_3/Week_3_1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+int t;
+
+class Stack {
+private:
+	int arr[20]{};
+	int tos;
+public:
+	Stack() {
+		tos = 0;
+	}
+
+	int size_() {
+		return tos;
+	}
+
+	bool empty() {
+		return tos == 0;
+	}
+
+	bool full() {
+		return tos == t;
+	}
+
+	void top() {
+		if (empty()) {
+			cout << "-1\n";
+			return;
+		}
+		cout << arr[tos - 1] << '\n';
+	}
+
+	void push(int x) {
+		if (full()) {
+			cout << "FULL\n";
+			return;
+		}
+		arr[tos++] = x;
+	}
+
+	void pop() {
+		if (empty()) {
+			cout << "-1\n";
+			return;
+		}
+		top();
+		tos--;
+	}
+};
+
+int main() {
+	cin >> t;
+	Stack S;
+	int N; cin >> N;
+
+	while (N--) {
+		string str; cin >> str;
+
+		if (str == "size") {
+			cout << S.size_() << '\n';
+		}
+
+		else if (str == "empty") {
+			cout << S.empty() << '\n';
+		}
+
+		else if (str == "full") {
+			cout << S.full() << '\n';
+		}
+
+		else if (str == "top") {
+			S.top();
+		}
+
+		else if (str == "push") {
+			int x; cin >> x;
+			S.push(x);
+		}
+
+		else if (str == "pop") {
+			S.pop();
+		}
+	}
+}
