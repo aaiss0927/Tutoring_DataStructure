5fa161ba02294c5eaec3c88100a9e1a506d940d0
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-21 20:49:33 +0900

    Week_1_1

---
+++ 24실습/Week_1/Week_1_1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+class Array {
+private:
+	int* arr;
+	int s;
+
+public:
+	Array(int s) {
+		this->s = s;
+		this->arr = new int[s];
+
+		for (int i = 0; i < s; i++) {
+			arr[i] = 0;
+		}
+	}
+
+	int at(int idx) {
+		return arr[idx];
+	}
+
+	void add(int idx, int value) {
+		for (int i = s - 2; i >= idx; i--) {
+			arr[i + 1] = arr[i];
+		}
+
+		arr[idx] = value;
+	}
+
+	void set(int idx, int value) {
+		arr[idx] = value;
+	}
+
+	void print() {
+		for (int i = 0; i < s; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	void remove(int idx) {
+		for (int i = idx + 1; i < s; i++) {
+			arr[i - 1] = arr[i];
+		}
+
+		arr[s - 1] = 0;
+	}
+
+	void count(int z) {
+		int cnt = 0;
+
+		for (int i = 0; i < s; i++) {
+			if (arr[i] == z) {
+				cnt++;
+			}
+		}
+
+		cout << cnt << '\n';
+	}
+};
+
+int main() {
+	int T, N; cin >> T >> N;
+	Array a(N);
+
+	while (T--) {
+		string cmd; cin >> cmd;
+
+		if (cmd == "at") {
+			int i; cin >> i;
+			cout << a.at(i) << '\n';
+		}
+
+		else if (cmd == "add") {
+			int i, z; cin >> i >> z;
+			a.add(i, z);
+		}
+
+		else if (cmd == "remove") {
+			int i; cin >> i;
+			a.remove(i);
+		}
+
+		else if (cmd == "set") {
+			int i, z; cin >> i >> z;
+			a.set(i, z);
+		}
+
+		else if (cmd == "count") {
+			int z; cin >> z;
+			a.count(z);
+		}
+
+		else if (cmd == "print") {
+			a.print();
+		}
+	}
+}
