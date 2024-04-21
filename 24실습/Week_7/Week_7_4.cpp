583851aeae3e3e86d4dbfa696d3347fd26a67add
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-21 20:38:27 +0900

    Week_7_4

---
+++ 24실습/Week_7/Week_7_4.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int temp;
+bool flag;
+
+class Node {
+private:
+	int value;
+	Node* parent;
+	vector<Node*> child_list;
+
+public:
+	int sum;
+
+	Node(int value, Node* parent) {
+		this->value = value;
+		this->parent = parent;
+	}
+
+	friend class Tree;
+};
+
+class Tree {
+public:
+	Node* root;
+	vector<Node*> node_list;
+
+	Tree() {
+		root = new Node(1, nullptr);
+		node_list.push_back(root);
+	}
+
+	int find(int value, vector<Node*>& list) {
+		for (int i = 0; i < list.size(); i++) {
+			if (list[i]->value == value) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	void insert_node(int par_value, int value) {
+		if (find(value, node_list) != -1) {
+			cout << "-1\n"; return;
+		}
+
+		int par_idx = find(par_value, node_list);
+		if (par_idx == -1) {
+			cout << "-1\n"; return;
+		}
+
+		Node* par_node = node_list[par_idx];
+		Node* new_node = new Node(value, par_node);
+
+		par_node->child_list.push_back(new_node);
+		node_list.push_back(new_node);
+	}
+
+	void cal_sum(Node* cur_node) {
+		while (cur_node != root) {
+			cur_node = cur_node->parent;
+			temp += cur_node->value;
+		}
+	}
+
+	void print(int value) {
+		int idx = find(value, node_list);
+		Node* cur_node = node_list[idx];
+
+		if (cur_node->child_list.empty()) {
+			cout << "D ";
+		}
+
+		else {
+			cout << "F ";
+		}
+
+		cout << cur_node->sum << '\n';
+	}
+};
+
+int main() {
+	Tree TR;
+	int N, M; cin >> N >> M;
+
+	for (int i = 0; i < N - 1; i++) {
+		int x, y; cin >> x >> y;
+		TR.insert_node(x, y);
+	}
+
+	for (int i = 0; i < TR.node_list.size(); i++) {
+		temp = 0;
+		TR.cal_sum(TR.node_list[i]);
+		TR.node_list[i]->sum = temp;
+	}
+
+	for (int i = 0; i < M; i++) {
+		int x; cin >> x;
+		TR.print(x);
+	}
+}
