8b8bd5e138d2173e8973bc3f8db5566ccda7e6c4
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-29 01:12:16 +0900

    Week_9_1

---
+++ 23실습/Week_9/Week_9_1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class sortedSeqPQ {
+private:
+	vector<int> seq;
+
+public:
+	void insert(int e) {
+		if (seq.empty()) {
+			seq.push_back(e);
+			return;
+		}
+
+		int idx = seq.size();
+
+		for (int i = 0; i < seq.size(); i++) {
+			if (e < seq[i]) {
+				idx = i;
+				break;
+			}
+		}
+
+		seq.insert(seq.begin() + idx, e);
+	}
+
+	int remove() {
+		int remove_value = seq.front();
+		seq.erase(seq.begin());
+		return remove_value;
+	}
+};
+
+int main() {
+	int T; cin >> T;
+
+	while (T--) {
+		vector<int> s;
+		sortedSeqPQ pq;
+		vector<int> add;
+		int N; cin >> N;
+
+		for (int i = 0; i < N; i++) {
+			int n; cin >> n;
+			s.push_back(n);
+		}
+
+		for (int i = 0; i < N; i++) {
+			pq.insert(s.front());
+			s.erase(s.begin());
+		}
+
+		for (int i = 0; i < N; i++) {
+			s.push_back(pq.remove());
+
+			if (s.size() > 1) {
+				add.push_back(s.front() + s.back());
+			}
+		}
+
+		for (int i = 0; i < s.size(); i++) {
+			cout << s[i] << ' ';
+		}
+
+		cout << '\n';
+
+		for (int i = 0; i < add.size(); i++) {
+			cout << add[i] << ' ';
+		}
+
+		cout << '\n';
+	}
+}
