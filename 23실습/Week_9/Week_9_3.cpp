43ad5b81d2855430692f5430df6055891e068e4c
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-29 00:36:16 +0900

    Week_9_3

--- 23실습/Week_9/Week_9_3.cpp
+++ 23실습/Week_9/Week_9_3.cpp
@@ -7,39 +7,11 @@ private:
 	vector<int> seq;
 
 public:
-	int size() {
-		return seq.size();
-	}
-
-	bool empty() {
-		return (seq.size() == 0);
-	}
-
 	void insert(int e) {
 		seq.push_back(e);
 	}
 
-	int max() {
-		if (empty()) {
-			return -1;
-		}
-
-		int maxi = seq[0];
-
-		for (int i = 0; i < seq.size(); i++) {
-			if (seq[i] > maxi) {
-				maxi = seq[i];
-			}
-		}
-
-		return maxi;
-	}
-
-	void removeMax() {
-		if (empty()) {
-			return;
-		}
-
+	int removeMax() {
 		int max_idx = 0;
 
 		for (int i = 0; i < seq.size(); i++) {
@@ -48,7 +20,9 @@ public:
 			}
 		}
 
+		int remove_value = seq[max_idx];
 		seq.erase(seq.begin() + max_idx);
+		return remove_value;
 	}
 };
 
@@ -63,12 +37,16 @@ int main() {
 
 		for (int i = 0; i < N; i++) {
 			int n; cin >> n;
-			pq.insert(n);
+			s.push_back(n);
 		}
 
 		for (int i = 0; i < N; i++) {
-			s.push_back(pq.max());
-			pq.removeMax();
+			pq.insert(s.front());
+			s.erase(s.begin());
+		}
+
+		for (int i = 0; i < N; i++) {
+			s.push_back(pq.removeMax());
 			
 			if (s.size() > 1) {
 				sub.push_back(s.front() - s.back());
