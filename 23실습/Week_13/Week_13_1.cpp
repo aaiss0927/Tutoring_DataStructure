d179c8c6f4ddd9f34c5e372ef09abae6b307743f
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-05-27 00:15:21 +0900

    Week_13_1

--- 23실습/Week_13/Week_13_1.cpp
+++ 23실습/Week_13/Week_13_1.cpp
@@ -198,18 +198,14 @@ public:
 		EdgeMatrix[dstMatrixId][srcMatrixId] = nullptr;
 	}
 
-	void isAdjacentOdd(int vertexId) {
+	void print_Adjacent(int vertexId) {
 		Vertex* curVertex = vList.findVertex(vertexId);
 		int matrixId = curVertex->matrixId;
-		int cnt = 0;
 
 		for (int i = 0; i < EdgeMatrix[matrixId].size(); i++) {
 			if (EdgeMatrix[matrixId][i] != nullptr)
-				cnt++;
+				cout << vList.findVertex_by_matrixId(i)->vertexId << ' ';
 		}
-
-		string result = (cnt % 2) ? "True" : "False";
-		cout << result << ' ' << cnt << '\n';
 	}
 
 	void minEdgeVertex() {
