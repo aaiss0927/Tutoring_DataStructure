f3ec706aff4c79cac6526a5f11e44f9baecfc699
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-06-03 01:02:36 +0900

    Week_14_1

---
+++ 23실습/Week_14/Week_14_1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> graph[1001];
+bool visited[1001];
+int cnt;
+
+void setup() {
+	for (int i = 0; i < 1001; i++) {
+		visited[i] = false;
+	}
+
+	cnt = 1;
+}
+
+void DFS(int a, int b) {
+	if (a == b) {
+		cout << cnt << '\n';
+		return;
+	}
+
+	int cur = a;
+
+	for (int i = 0; i < graph[a].size(); i++) {
+		int next = graph[a][i];
+
+		if (visited[next])
+			continue;
+
+		cnt++;
+		visited[next] = true;
+		DFS(next, b);
+	}
+}
+
+int main() {
+	int N, M, Q; cin >> N >> M >> Q;
+
+	for (int i = 0; i < M; i++) {
+		int u, v; cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+
+	for (int i = 0; i < Q; i++) {
+		int a, b; cin >> a >> b;
+
+		setup();
+		visited[a] = true;
+		DFS(a, b);
+	}
+}
