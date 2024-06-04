96f4133e1ce134013f6a54923b592c8ae94399a2
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-06-04 21:53:01 +0900

    Week_13_1

---
+++ 24실습/Week_13/Week_13_1.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+bool graph[10001][10001];
+
+int main() {
+    int T; cin >> T;
+    bool vertex[10001] = { false, };
+
+    while (T--) {
+        string cmd; cin >> cmd;
+
+        if (cmd == "insertVertex") {
+            int s; cin >> s;
+
+            if (vertex[s]) {
+                cout << "Exist\n";
+                continue;
+            }
+
+            vertex[s] = true;
+        }
+
+        else if (cmd == "insertEdge") {
+            int s, d; cin >> s >> d;
+
+            if (graph[s][d] || graph[d][s]) {
+                cout << "Exist\n";
+                continue;
+            }
+
+            graph[s][d] = true;
+            graph[d][s] = true;
+        }
+
+        else if (cmd == "eraseVertex") {
+            int s; cin >> s;
+
+            for (int i = 1; i <= 10000; i++) {
+                graph[s][i] = false;
+                graph[i][s] = false;
+            }
+
+            vertex[s] = false;
+        }
+
+        else if (cmd == "eraseEdge") {
+            int s, d; cin >> s >> d;
+
+            if (!graph[s][d] || !graph[d][s]) {
+                cout << "None\n";
+                continue;
+            }
+
+            graph[s][d] = false;
+            graph[d][s] = false;
+        }
+
+        else if (cmd == "isAdjacent") {
+            int s, d;  cin >> s >> d;
+            int cnt_1 = 0, cnt_2 = 0;
+
+            if (graph[s][d])
+                cout << "True ";
+
+            else
+                cout << "False ";
+
+            for (int i = 1; i <= 10000; i++) {
+                if (graph[s][i])
+                    cnt_1++;
+
+                if (graph[d][i])
+                    cnt_2++;
+            }
+
+            if (cnt_1 < cnt_2)
+                cout << s;
+
+            else if (cnt_1 > cnt_2)
+                cout << d;
+
+            else {
+                if (s < d)
+                    cout << s;
+
+                else
+                    cout << d;
+            }
+            cout << '\n';
+        }
+
+        else if (cmd == "minVertexInfo") {
+            int min = 10001;
+            
+            for (int i = 1; i <= 10000; i++) {
+                if (vertex[i]) {
+                    min = i;
+                    break;
+                }
+            }
+
+            if (min == 10001) {
+                cout << "-1\n";
+                continue;
+            }
+
+            int cnt = 0;
+
+            for (int i = 1; i <= 10000; i++) {
+                if (graph[min][i])
+                    cnt++;
+            }
+
+            cout << min << ' ' << cnt << '\n';
+        }
+    }
+}
